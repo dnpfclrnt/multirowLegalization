d342928aa163bbd34b0931e84b4f10d9a530b48d
Chanhee Jeon <dnpfclrnt@gmail.com>
2022-10-05 17:56:45 +0900

    TEST

--- src/lg/legality.cpp
+++ src/lg/legality.cpp
@@ -6,6 +6,10 @@ void placmenet_map_init(database_ptr data)
     int rowLen = data->die_data->rowLen;
     int numRows = data->die_data->repeatCount;
 
+    unsigned int mem_used = rowLen * numRows * sizeof(bool);
+    mem_used = mem_used >> 3;
+    printf("PROC: Using = %u kB\n", mem_used);
+
     data->die_data->placementMap = (bool**)malloc(sizeof(bool*) * numRows);
     for (int i = 0; i < numRows; i++)
     {
